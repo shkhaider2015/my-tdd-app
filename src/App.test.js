de6a722f0ddd42ebb31a82b5b61d7ac433289664
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-07-14 03:33:41 +0500

    sadas

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {mount} from "enzyme"
+// import Counter from './Counter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe("Counter Testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  })
+
+  test('renders the title of counter', () => {
+    // const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toContain("This is counter app")
+  });
+
+  test("render a button with text of `increment`", () => {
+    // const wrapper = shallow(<App />)
+    expect(wrapper.find("#increment-btn").text()).toBe('Increment');
+  })
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find('#counter-value').text()).toBe("0");
+  })
+
+  test("render the click event of increement/decrement button and increment/decrement counter value", () => {
+    wrapper.find('#increment-btn').simulate('click');
+    expect(wrapper.find("#counter-value").text()).toBe("1")
+
+    wrapper.find('#decrement-btn').simulate('click');
+    expect(wrapper.find("#counter-value").text()).toBe("0")
+  })
+
+})
+
---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Counter = () =>
+{
+    
+  const [counter, setCounter] = useState(0);
+    return <div>
+         <h1>
+      This is counter app
+      </h1>
+      <div id="counter-value" >
+          {counter}
+      </div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)} >
+        Increment
+      </button>
+      <button
+       id="decrement-btn" 
+       onClick={() => setCounter(counter - 1)}
+       disabled={counter === 0} >
+        Decrement
+      </button>
+    </div>
+}
+
+export default Counter;
